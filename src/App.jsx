706acd618fccaddc1600da0652c379fb45213bec
Yosuke Onoue <onoue@likr-lab.com>
2022-12-30 13:42:43 +0900

    overlay contour

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,19 @@ function ZoomableSVG({ children, width, height }) {
   );
 }
 
-function Map() {
-  console.log("log");
-  const left = d3.min(data, (region) => d3.min(region.polygon, (p) => p[0]));
-  const right = d3.max(data, (region) => d3.max(region.polygon, (p) => p[0]));
-  const top = d3.min(data, (region) => d3.min(region.polygon, (p) => p[1]));
-  const bottom = d3.max(data, (region) => d3.max(region.polygon, (p) => p[1]));
+function Map({ startIndex, stopIndex }) {
+  const left = d3.min(data.regions, (region) =>
+    d3.min(region.polygon, (p) => p[0])
+  );
+  const right = d3.max(data.regions, (region) =>
+    d3.max(region.polygon, (p) => p[0])
+  );
+  const top = d3.min(data.regions, (region) =>
+    d3.min(region.polygon, (p) => p[1])
+  );
+  const bottom = d3.max(data.regions, (region) =>
+    d3.max(region.polygon, (p) => p[1])
+  );
   const width = right - left;
   const height = bottom - top;
   const aspectRatio = height / width;
@@ -50,14 +57,22 @@ function Map() {
     .range([0, contentHeight]);
   const fontSizeScale = d3
     .scaleSqrt()
-    .domain(d3.extent(data, (region) => region.count))
-    .range([1, 14]);
+    .domain(d3.extent(data.regions, (region) => d3.sum(region.count)))
+    .range([3, 14]);
   const color = d3.scaleOrdinal(d3.schemeTableau10);
+  const kde = d3
+    .contourDensity()
+    .x((region) => scaleX(d3.polygonCentroid(region.polygon)[0]))
+    .y((region) => scaleY(d3.polygonCentroid(region.polygon)[1]))
+    .weight((region) =>
+      d3.sum(d3.range(startIndex, stopIndex), (i) => region.count[i])
+    )
+    .size([contentWidth, contentHeight]);
   return (
     <ZoomableSVG width={displayWidth} height={displayHeight}>
       <g transform={`translate(${displayMargin},${displayMargin})`}>
         <g>
-          {data.map((region) => {
+          {data.regions.map((region) => {
             const path = d3.path();
             path.moveTo(
               scaleX(region.polygon[0][0]),
@@ -82,14 +97,23 @@ function Map() {
           })}
         </g>
         <g>
-          {data.map((region) => {
+          {kde(data.regions).map((polygon, i) => {
+            return (
+              <g key={i}>
+                <path d={d3.geoPath()(polygon)} fill="white" opacity="0.1" />
+              </g>
+            );
+          })}
+        </g>
+        <g>
+          {data.regions.map((region) => {
             const [cx, cy] = d3.polygonCentroid(region.polygon);
             return (
               <g key={region.tag}>
                 <text
                   x={scaleX(cx)}
                   y={scaleY(cy)}
-                  fontSize={fontSizeScale(region.count)}
+                  fontSize={fontSizeScale(d3.sum(region.count))}
                   fontWeight="bold"
                   textAnchor="middle"
                   dominantBaseline="central"
@@ -106,5 +130,43 @@ function Map() {
 }
 
 export default function App() {
-  return <Map />;
+  const [startIndex, setStartIndex] = useState(0);
+  const [stopIndex, setStopIndex] = useState(data.months.length);
+  return (
+    <div>
+      <div>
+        <select
+          value={startIndex}
+          onChange={(event) => {
+            setStartIndex(+event.target.value);
+          }}
+        >
+          {data.months.map((month, i) => {
+            return (
+              <option key={month} value={i}>
+                {month}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          value={stopIndex}
+          onChange={(event) => {
+            setStopIndex(+event.target.value);
+          }}
+        >
+          {data.months.map((month, i) => {
+            return (
+              <option key={month} value={i + 1}>
+                {month}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div>
+        <Map startIndex={startIndex} stopIndex={stopIndex} />
+      </div>
+    </div>
+  );
 }
